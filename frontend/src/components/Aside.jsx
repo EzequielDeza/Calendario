e15d33c271b40bdfaa4bb9be45587ad0bfa16483
Ezequiel Deza <401006@sistemas.frc.utn.edu.ar>
2025-07-13 20:21:56 -0300

    Tailwind utilizado correctamente desde el CMD (no por instalacion local, osea que no esta listo para produccion)

---
+++ frontend/src/components/Aside.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Aside() {
+
+    return (
+        <>
+        <div className="bg-gray-200 p-4">
+          Componente Aside
+        </div>
+        </>
+    )
+}
+
+export default Aside
---
+++ frontend/src/components/Calendario.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Calendario() {
+
+  return (
+    <>
+      <div className="bg-white p-4 shadow-md">
+        Componente Calendario
+      </div>
+    </>
+  )
+}
+
+export default Calendario
